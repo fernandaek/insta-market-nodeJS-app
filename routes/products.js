944b2c18a4acc2d16bb52241b55eada821960f23
Fernanda Ek <fernandaek@icloud.com>
2019-08-22 10:05:06 +0000

    Added upload function

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,29 @@ var Product = require("../models/product");
 var middleware = require("../middleware");
 var multer = require("multer");
 
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'fernandaek', 
+  api_key: 288584932748768, 
+  api_secret: "OBoyJzz-6JC4-WnODg9KA9-uuqw"
+});
+
+
 //INDEX ROUTE - Show all products
 router.get("/products", (request, response) =>{
 	//console.log(request.user);
@@ -39,29 +62,24 @@ router.get("/products/:id", (request, response) => {
 });
 
 //CREATE ROUTE - Add new product to DB
-router.post("/products", middleware.isLoggedIn, (request, response) => {
+router.post("/products", middleware.isLoggedIn, upload.single('image'), (request, response) => {
 //get data from form
-		var name = request.body.name;
-		var price = request.body.price;
-		var image = request.body.image;
-		var desc = request.body.description;
-		var location = request.body.location;
-		var coordinates = request.body.coordinates;
-		var author = {
-			id: request.user._id,
-			username: request.user.username
-		}
-		var newProduct = {name: name, price: price, image: image, description: desc,location: location, coordinates: coordinates, author: author}
-		
-		//products.push(newProduct);
-		Product.create(newProduct, (err, newlyCreated)=>{
-			if(err){
-				console.log(err);
-			}else{
-	//Redirect  back to products page
-		response.redirect("/products");
-			}
-		});
+	cloudinary.uploader.upload(request.file.path, function(result) {
+  // add cloudinary url for the image to the product object under image property
+  request.body.product.image = result.secure_url;
+  // add author to product
+  request.body.product.author = {
+    id: request.user._id,
+    username: request.user.username
+  }
+  Product.create(request.body.product, function(err, product) {
+    if (err) {
+      request.flash('error', err.message);
+      return response.redirect('back');
+    }
+    response.redirect('/products/' + product.id);
+ 	 });
+  });
 });
 
 //EDIT a product route
